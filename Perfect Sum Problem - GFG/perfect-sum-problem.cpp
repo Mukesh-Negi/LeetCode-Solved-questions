ddeee15ea716091e31a0cfb57b9f6060adfcd72e
Mukesh-Negi <107361627+Mukesh-Negi@users.noreply.github.com>
2023-07-29 11:13:29 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -4,33 +4,43 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
-    int m = 1e9 + 7;
-    int f( int idx, int arr[], int target, vector <vector<int>> &dp)
-    {
-        if( idx == -1)
-        {
-            if( target == 0) return 1;
-            return 0;
-        }
-        
-        if( dp[idx][target] != -1) return dp[idx][target];
-        
-        int notPick = f(idx-1, arr, target, dp) % m;
-        int pick = 0;
-        if( arr[idx] <= target) 
-        {
-            pick = f(idx-1, arr, target-arr[idx], dp) %m;
-        }
-        
-        return dp[idx][target] =  pick + notPick;
-    }
+
 	public:
 	int perfectSum(int arr[], int n, int sum)
 	{
-	    vector <vector<int>> dp(n, vector<int> (sum+1, -1));
-	    int ans = f(n-1, arr, sum, dp);
-	    ans %= m;
-	    return ans;
+	    int m = 1e9 + 7;
+	    // tabulation solution
+	    vector <vector<int>> dp(n, vector<int> (sum+1, 0));
+	    
+	    dp[0][0] = 1;// if target is 0 it means we have already picked req. elements in subset.
+	    
+	    if(arr[0] <= sum)
+	    {
+	        /*
+	        if value is 0 at 0th index then target can be achieved by 
+	        both ways : picking & not-picking that element.
+	        so we are doing dp[0][arr[0]]++ so that in case if arr[0]= 0 its value will
+	        be updated to 2
+	        */
+	        
+	        dp[0][arr[0]]++;
+	    }
+	    
+	    // now we will fill the remaining rows using these rows.
+	    for( int i = 1; i < n; i++)
+	    {
+	        for( int target = 0; target <= sum; target++)
+	        {
+	            int notPick = dp[i-1][target] %m;
+	            int pick = 0;
+	            if( arr[i] <= target)
+	            {
+	                pick = dp[i-1][target-arr[i]] %m;
+	            }
+	            dp[i][target] = pick + notPick;
+	        }
+	    }
+	    return dp[n-1][sum] % m;
 	}
 	  
 };

f9a2b5851d8326ee1a59c238cbe9b3cd43915597
Mukesh-Negi <107361627+Mukesh-Negi@users.noreply.github.com>
2023-07-30 11:49:42 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -4,56 +4,49 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    private:
-    int m = 1e9 + 7;
-    int f( int idx, int target, vector<int> &arr, vector <vector<int>> &dp)
-    {
-        if( idx == 0)
-        {
-            if( target == 0 && arr[0] == 0) return 2;
-            
-            if( target == 0) return 1;// target is 0 but arr[0] is not 0
-            
-            if( arr[0] == target) return 1;
-            
-            return 0;
-        }
-        
-        if( dp[idx][target] != -1) return dp[idx][target];
-        
-        int notPick = f(idx-1, target, arr, dp);
-        
-        int pick = 0;
-        
-        if( arr[idx] <= target)
-        {
-            pick = f(idx-1, target-arr[idx], arr, dp);
-        }
-        
-        return dp[idx][target] = (pick + notPick) % m;
-    }
   public:
+  int m = int(1e9 + 7);
     int countPartitions(int n, int d, vector<int>& arr) {
         
-        // using striver's approach
-        
-        /*
-        s1 - s2 = d    && s1 > s2
-        s1 + s2 = s
-        s2 = (s - d)/2
-        */
+        // solving using tabulation
         
         int totSum = accumulate(arr.begin(), arr.end(), 0);
-        // edge cases
-        if( totSum - d < 0) return 0;
         
-        if( (totSum - d) % 2 == 1) return 0;
+        // s2 = (totSum - d)/2
         
-        int target = (totSum-d)/2;
+        if( totSum -d  < 0 ) return 0;
         
-        vector <vector<int>> dp(n, vector<int>(target + 1, -1) );
+        if( (totSum -d) % 2 == 1 ) return 0;
         
-        return  f(n-1,target, arr, dp);
+        int s2 = (totSum-d)/2;
+        
+        vector<vector<int>> dp(n, vector<int> (s2+1, 0));
+        
+        if( arr[0] == 0) dp[0][0] = 2;
+        
+        else dp[0][0] = 1;
+        
+        if( arr[0] <= s2 && arr[0] != 0) // we have only handled for arr[0] = 0
+        {
+            dp[0][arr[0]] = 1;
+        }
+        
+        for( int i = 1; i < n; i++)
+        {
+            for( int tar = 0; tar <=s2; tar++)
+            {
+                int notPick = dp[i-1][tar];
+                int pick = 0;
+                if( arr[i] <= tar)
+                {
+                    pick = dp[i-1][tar-arr[i]];
+                }
+                
+                dp[i][tar] = (pick + notPick ) %m;
+            }
+        }
+        return dp[n-1][s2];
+            
     }
 };
 
